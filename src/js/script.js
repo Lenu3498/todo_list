cd96a2d1dad683e464a338d55e54b06b8051c684
Lena <l.ernst3498@gmail.com>
2021-03-06 16:11:22 +0100

    adjust font size

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+
+const inputValue = document.querySelector(".newInput").value;
+const form = document.querySelector(".taskAdder");
+form.addEventListener('submit', addNewTodo)
+
+const addNewTodo = function() {
+
+}
+console.log(inputValue);
